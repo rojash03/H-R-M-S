c4c163f9b5eff259123f88ff1052fe98f1d6cb5e
rojash03 <np05cp4a240283@iic.edu.np>
2025-07-18 20:28:47 +0545

    leave route update

--- Routes/leave.route.js
+++ Routes/leave.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllLeaves, getLeaveById, createLeave, updateLeave } from '../controller/leave.controller.js';
+import { getAllLeaves, getLeaveById, createLeave, updateLeave } from '../Controller/leave.controller.js';
 import { verifyAdminorManager } from '../mildware/verify.token.js';
 
 const router = express.Router();
